1e112ffaaec8cb20b68c5b125f15005d418d4c96
Piyush kumar <piyush2004.dev@gmail.com>
2024-11-15 11:00:33 +0530

    wrote a global error handler

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 const app = express();
 import userRouter from "./routes/user.routes.js";
+import ApiError from "./utils/ApiError.js";
 
 app.use(
   cors({
@@ -19,5 +20,26 @@ app.use(cookieParser());
 
 // routes
 app.use("/api/users", userRouter);
+
+// global error handler
+app.use((err, _, res, __) => {
+  if (err instanceof ApiError) {
+    res.status(err.statusCode).json({
+      error: {
+        statusCode: err.statusCode,
+        message: err.message,
+        success: err.success,
+        data: err.data,
+        errors: err.errors,
+      },
+    });
+  } else {
+    // Default error handler for unexpected errors
+    res.status(err.status || 500).json({
+      message: err.message || "Internal Server Error",
+      success: false,
+    });
+  }
+});
 
 export { app };
