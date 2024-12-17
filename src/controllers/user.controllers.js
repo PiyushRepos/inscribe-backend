f3644adae692ea11ab0ce8f49dbb0da1980cb00b
Piyush kumar <piyush2004.dev@gmail.com>
2024-12-17 20:45:47 +0530

    deployed

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -89,7 +89,6 @@ const loginUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   };
 
